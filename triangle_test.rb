6984fb438bfa4146609d3bf2b6d7939565d68e6c
Bella <woo.bella@gmail.com>
2015-05-18 15:53:03 -0400

    Adding solutions for triangle test

---
+++ triangle_test.rb
@@ -0,0 +1,57 @@
+# 1. Copy this file from the course notes repo into a new repository
+# 2. Run `ruby triangle_test.rb`. You should see 6 error messages.
+# 3. Implement the Triangle class until all 6 tests are passing.
+require "minitest/autorun"
+
+class Triangle
+  # Add your code here
+  def initialize(a,b,c)
+    @Triangle = Triangle
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind
+    if @a == @b && @b == @c
+      return :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      return :isosceles
+    else return :scalene
+    end
+  end
+  def perimeter
+    return @a + @b + @c
+  end
+end
+
+class TestMeme < Minitest::Test
+  def test_equilateral
+    t = Triangle.new(5,5,5)
+    assert_equal :equilateral, t.kind
+  end
+
+  def test_isosceles
+    s = Triangle.new(2,2,3)
+    assert_equal :isosceles, s.kind
+  end
+
+  def test_other_isosceles
+    t = Triangle.new 3,4,3
+    assert_equal :isosceles, t.kind
+  end
+
+  def test_scalene
+    t = Triangle.new 6,7,8
+    assert_equal :scalene, t.kind
+  end
+
+  def test_perimeter_right
+    t = Triangle.new(3,4,5)
+    assert_equal 12, t.perimeter
+  end
+
+  def test_perimeter_eq
+    t = Triangle.new 10,20,30
+    assert_equal 60, t.perimeter
+  end
+end
